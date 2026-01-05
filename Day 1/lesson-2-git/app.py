be47f5993c725cffd1f221b6221851316d7e0c02
AKIA5Q7ZPD3ASTCV3QA4 <ZnCF/SjJ6T+eamDEJXLYoePJSaGDSlDg8FEDLEdTSXdSyrpSaW0teccc49s=>
2026-01-05 20:22:12 +0530

    data set chaged form 0.2 to 0.3

--- Day 1/lesson-2-git/app.py
+++ Day 1/lesson-2-git/app.py
@@ -18,7 +18,7 @@ X = df.drop(columns=['Target'])
 y = df['Target']
 
 # Split data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Scale the data (optional, improves performance for some models)
 scaler = StandardScaler()
